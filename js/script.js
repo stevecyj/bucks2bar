e817c8c299d1ae810ab458d392e6c7d5e770670a
stevecyj <ycsteven.tsao@gmail.com>
2023-12-16 17:35:16 +0800

    feat: add bar chart functionality using Chart.js
    - Added a script tag to import the Chart.js library in index.html
    - Changed the id of the chart tab link in index.html
    - Added a canvas element with id "myChart" in index.html
    - Added JavaScript code to initialize and update a bar chart in script.js
    - Added a function named "updateChart" in script.js to update the chart data
    - Added an event listener to the chart tab link in script.js to call the "updateChart" function when clicked
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,1 +1,99 @@
-// This file is intentionally left blank
+// 假設您已經有了以下的數據
+var months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+var income = [];
+var expenses = [];
+
+for (var i = 0; i < months.length; i++) {
+  var month = months[i].toLowerCase();
+  var incomeInput = document.getElementById(month + "-income");
+  var expensesInput = document.getElementById(month + "-expenses");
+
+  if (incomeInput && incomeInput.value) {
+    income.push(Number(incomeInput.value));
+  } else {
+    income.push(0);
+  }
+
+  if (expensesInput && expensesInput.value) {
+    expenses.push(Number(expensesInput.value));
+  } else {
+    expenses.push(0);
+  }
+}
+
+var ctx = document.getElementById("myChart").getContext("2d");
+var myChart = new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: months,
+    datasets: [
+      {
+        label: "Income",
+        data: income,
+        backgroundColor: "rgba(75, 192, 192, 0.2)",
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderWidth: 1,
+      },
+      {
+        label: "Expenses",
+        data: expenses,
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        borderColor: "rgba(255, 99, 132, 1)",
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
+function updateChart() {
+  // 獲取新的收入和支出數據
+  var income = [];
+  var expenses = [];
+  for (var i = 0; i < months.length; i++) {
+    var month = months[i].toLowerCase();
+    var incomeInput = document.getElementById(month + "-income");
+    var expensesInput = document.getElementById(month + "-expenses");
+
+    if (incomeInput && incomeInput.value) {
+      income.push(Number(incomeInput.value));
+    } else {
+      income.push(0);
+    }
+
+    if (expensesInput && expensesInput.value) {
+      expenses.push(Number(expensesInput.value));
+    } else {
+      expenses.push(0);
+    }
+  }
+
+  // 更新圖表的數據
+  myChart.data.datasets[0].data = income;
+  myChart.data.datasets[1].data = expenses;
+  myChart.update();
+}
+
+document.getElementById("chart-tab").addEventListener("click", function () {
+  // 在這裡運行您的代碼
+  updateChart();
+});
